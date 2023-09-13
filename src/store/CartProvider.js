c17feb933f4a510ebf1b4d626ac2f75b1fbd2b0c
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-13 16:51:33 +0100

    editted cartprovider.js so it brings in props

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,7 +5,7 @@ import React from "react";
 
 import CartContext from "./cart-context";
 
-const CartProvider = () => {
+const CartProvider = props => {
 
     const addItemToCartHandler = item => {};
 
