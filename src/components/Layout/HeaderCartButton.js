6270fc3692baee19ea4650ee0266c6ccb7cb80e3
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-14 14:13:04 +0100

    using context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
+import CartContext from "../../store/cart-context";
+import classes from "./HeaderCartButton.module.css";
+import Cart from "../Cart/Cart";
 
-const HeaderCartButton = props => {
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            {/* will wrap icon */}
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            {/* will wrap text */}
-            <span>Your Cart</span>
-            {/* will wrap badge */}
-            <span className={classes.badge}>3</span>
-        </button>
+const HeaderCartButton = (props) => {
+  // header will be reevaluated when updated
+  // updated in CartProvider component
+  const cartCtx = useContext(CartContext);
 
-    );
+  // .reduce() allows transformation of array into single value
+  // takes 2 arguments
+  const numOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      {/* will wrap icon */}
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      {/* will wrap text */}
+      <span>Your Cart</span>
+      {/* will wrap badge */}
+      <span className={classes.badge}>{numOfCartItems}</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
 
-// so the function received on onClick in headerCartButton is forwarded to 
+// so the function received on onClick in headerCartButton is forwarded to
 // the onClick prop on the button
 
 // chain of props
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,13 @@
 import classes from './Input.module.css';
 
 const Input = props => {
-    return
+    return (
         <div className={classes.input}>
             <label htmlFor={props.input.id} >{props.label}</label>
             <input {...props.input} /> 
                 {/* id={props.input.id} this is automatically added */}
         </div>
+    );
 };
 
 export default Input;
