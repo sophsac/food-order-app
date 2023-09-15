e4be0334417d00cfbde84b23bbc18ecf37cfb0e8
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-15 11:33:37 +0100

    point removeItem in removehandler in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,7 +15,9 @@ const Cart = (props) => {
   // render Order button if there's items in the cart
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount: 1});
