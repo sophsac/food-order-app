fa63b44e3391f91e60cccedb8310ff9024a0b269
Sophia Sacatani <ssacatani@adaptavist.com>
2023-09-15 11:18:07 +0100

    to check if the item is already in the cart, and update the amount of that item or add a new one to the list

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,57 +1,79 @@
-// to manage the cart-context data 
+// to manage the cart-context data
 // to provide that context to all components that want access to it
 
 import React, { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
-const defaultCartState ={
-    items: [],
-    totalAmount: 0
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
-    if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.totalAmount;
-        return {
-            items: updatedItems,
-            totalAmount: updatedTotalAmount
+  if (action.type === "ADD") {
+    const updatedTotalAmount =
+    state.totalAmount + action.item.price * action.item.totalAmount;
+
+    // to check if the item is already in the cart
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    // accesses the index if it exists
+    const existingCartItem = state.items[existingCartItemIndex]
+    let updatedItems;
+
+    if (existingCartItem) {
+        const updatedItem = {
+            ...existingCartItem,
+            amount: existingCartItem.amount + action.item.amount
         };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+        updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
-    return defaultCartState;
+  }
+  return defaultCartState;
 };
 
-const CartProvider = props => {
+const CartProvider = (props) => {
+  // second element function allows to dispatch an action to the reducer
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-    // second element function allows to dispatch an action to the reducer
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
 
-    const addItemToCartHandler = (item) => {
-        dispatchCartAction({type: 'ADD', item: item});
-    };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
-    const removeItemFromCartHandler = (id) => {
-        dispatchCartAction({type: 'REMOVE', id: id})
-    };
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
 
-    const cartContext = {
-        items: cartState.items,
-        totalAmount: cartState.totalAmount,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    };
-
-    return (
-        <CartContext.Provider value={cartContext}>
-            {props.children}
-        </CartContext.Provider>
-    );
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
 
-// {props.children} allows wrapping of any component that should get access to this context with 
+// {props.children} allows wrapping of any component that should get access to this context with
 // CartProvider component
 
 // need to wrap all components with CartProvider that need access to the cart
